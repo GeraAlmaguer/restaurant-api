5485626c6d704ebfaf9643785d604294a85ad5bc
Gerardo Almaguer <gerardo.almaguer.ramos@u-erre.mx>
2023-04-19 00:08:02 -0600

    routing

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.port | 3000;
+
+const homeRoutes = require('./routes/home.routes');
+const reservationsRoutes = require('./routes/home.routes');
+const usersRoutes = require('./routes/users.routes');
+
+app.use('/', homeRoutes);
+app.use('/reservations', reservationsRoutes);
+app.use('/users', usersRoutes);
+
+app.listen(port, () => console.log('Server listening at port ' + port));
+
