45a632bd2c96627d0b238c5afe4e2c67b88ed6db
Gerardo Almaguer <gerardo.almaguer.ramos@u-erre.mx>
2023-04-19 00:35:44 -0600

    rutas API

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const port = process.env.port | 3000;
 
 const homeRoutes = require('./routes/home.routes');
-const reservationsRoutes = require('./routes/home.routes');
+const reservationsRoutes = require('./routes/reservations.routes');
 const usersRoutes = require('./routes/users.routes');
 
 app.use('/', homeRoutes);
 app.use('/reservations', reservationsRoutes);
 app.use('/users', usersRoutes);
 
-app.listen(port, () => console.log('Server listening at port ' + port));
+app.listen(port, () => console.log('Server listenig at port ' + port));
 
